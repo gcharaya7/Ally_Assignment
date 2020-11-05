c209d944594767a7ee80a4fc905077458a6692fe
Gourav <Gourav@go-mmt.com>
2020-11-05 21:11:46 +0530

    fixes

--- src/actions/landing/index.js
+++ src/actions/landing/index.js
@@ -3,20 +3,15 @@ import {
   UPDATE_LANDING_DATA,
   UPDATE_DATA_LOAD_STATUS,
 } from "./actionConstants";
-import { isDateToday, isTimingCurrent } from "../../utils/commonUtils";
 import { API_CALL_STATUS } from "../../constants";
 
 export const loadLandingData = () => async (dispatch, _getState) => {
-  console.log("loadLandingData");
   dispatch({
     type: UPDATE_DATA_LOAD_STATUS,
     dataLoadStatus: API_CALL_STATUS.INPROGRESS,
   });
   const landingData = await getLandingData();
-  console.log("landingData", landingData);
-
   const list = [];
-  landingData.data;
   landingData.data.forEach((item) => {
     if (!item.parent_objective_id) {
       list.push(item);
@@ -24,7 +19,6 @@ export const loadLandingData = () => async (dispatch, _getState) => {
       let index = list.findIndex(
         (listItem) => listItem.id === item.parent_objective_id
       );
-      console.log("landingData", index);
       if (index !== -1) {
         if (!list[index]["childern"]) {
           list[index]["childern"] = [];
@@ -34,7 +28,6 @@ export const loadLandingData = () => async (dispatch, _getState) => {
     }
   });
 
-  console.log("UPDATE_LANDING_DATA", list);
   dispatch({
     type: UPDATE_LANDING_DATA,
     landingData: list,
